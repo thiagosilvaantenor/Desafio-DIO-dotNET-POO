a841308cfa057f84a9ccf98ae17a40bac468c277
Thiago Antenor <thiagoantenor31@gmail.com>
2023-12-08 12:34:38 -0300

    Desafio concluido, metodos, heranças e construtores finalizados

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+//Teste do Iphone:
+Iphone iphone = new Iphone(numero: "11122233", modelo: "Modelo 1", imei: "789456", memoria: 128);
+Console.WriteLine($"Iphone criado com sucesso!");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Youtube");
+
+
+//Teste do Nokia:
+Nokia nokia = new Nokia(numero: "11122233", modelo: "Modelo 1", imei: "789456", memoria: 128);
+Console.WriteLine("---------");
+Console.WriteLine($"Nokia criado com sucesso!");
+nokia.Ligar();
+nokia.InstalarAplicativo("Netflix");
+nokia.ReceberLigacao();
